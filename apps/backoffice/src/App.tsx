72b773197e768349e4c85422ecb378a368c635f8
Bernard Laetitia <97946771+laebernard@users.noreply.github.com>
2026-02-06 10:39:37 +0100

    init projet

---
+++ apps/backoffice/src/App.tsx
@@ -0,0 +1,10 @@
+import {hello} from "@BoucheeEtDragons/design-system";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Backoffice</h1>
+      <p>{hello()}</p>
+    </div>
+  )
+}
---
+++ packages/design-system/dist/index.mjs
@@ -0,0 +1,6 @@
+// src/index.ts
+var hello = () => "Hello world";
+export {
+  hello
+};
+//# sourceMappingURL=index.mjs.map
