0965373a1505d3b209fd0509df3bbece8939dd8e
Bernard Laetitia <97946771+laebernard@users.noreply.github.com>
2026-03-03 17:16:12 +0100

    init page Menu

--- apps/backoffice/src/main.tsx
+++ apps/backoffice/src/main.tsx
@@ -1,11 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import MenuPage from './pages/menu/menu.tsx'
+import Login from './pages/login/login.tsx'
+import SystemDesign from './pages/system-design.tsx'
 import './index.css'
 import App from './App.tsx'
 import "./index.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MenuPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/system-design" element={<SystemDesign />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
