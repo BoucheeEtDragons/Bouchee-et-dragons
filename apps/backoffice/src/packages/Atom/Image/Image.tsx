d4302b5990c0da514f09d09801749a817ed1cbdb
Bernard Laetitia <97946771+laebernard@users.noreply.github.com>
2026-03-03 17:59:46 +0100

    fix: taille image dans menuItem

--- apps/backoffice/src/packages/Atom/Image/Image.tsx
+++ apps/backoffice/src/packages/Atom/Image/Image.tsx
@@ -1,22 +1,31 @@
 import React from "react";
 import "./image.css";
+import clsx from "clsx";
 
 interface Props {
     src: string,
     alt?: string,
+    className?: string,
+    width?: string|number,
+    height?: string|number,
     [key: string]: any,
 }
 
 export const Image: React.FC<Props> = ({
     src,
     alt,
+    className,
+    width,
+    height,
     ...props
 }) => {
     return (
         <img
-            className="bd-image"
+            className={clsx('bd-image', className)}
             src={src}
             alt={alt}
+            width={width}
+            height={height}
             {...props}
         />
     );
